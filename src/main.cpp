423f553a64602f3cf8e9ac1353ceb4752a1da40a
RushanCPP <rushan.davlitshin.0809@gmail.com>
2022-02-12 21:42:07 +0500

    Added and some tested key callback and size callback

--- src/main.cpp
+++ src/main.cpp
@@ -2,30 +2,55 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-int main(void)
+int my_window_size_x = 640;
+int my_window_size_y = 480;
+
+void glfwWindowSizeCallback(GLFWwindow* window, int n_width, int n_height) 
 {
-    GLFWwindow* window;
+    my_window_size_x = n_width;
+    my_window_size_y = n_height;
+    glViewport(0, 0, my_window_size_x, my_window_size_y);
+}
+void glfwKeyCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+int main() 
+{
 
     /* Initialize the library */
     if (!glfwInit())
+    {
+        std::cout << "GLFW PROBLEMS!";
         return -1;
-
+    }
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    GLFWwindow* window = glfwCreateWindow(my_window_size_x, my_window_size_y, 
+                                          "Hello World", NULL, NULL);
+    if (!window) 
     {
         glfwTerminate();
         return -1;
     }
-
+    glfwSetWindowSizeCallback(window, glfwWindowSizeCallback);
+    glfwSetKeyCallback(window, glfwKeyCallback);
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-	if (!gladLoadGL()) {
+	if (!gladLoadGL()) 
+    {
 		std::cout << "GLAD PROGLEMS!";
 		return -1;
 	}
-	std::cout << "OpenGL: " << GLVersion.major << "." << GLVersion.minor << std::endl;
+
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+	std::cout << glGetString(GL_VERSION) << std::endl;
+    
     /* Loop until the user closes the window */
     glClearColor(0, 1, 0, 1);
 	while (!glfwWindowShouldClose(window))
